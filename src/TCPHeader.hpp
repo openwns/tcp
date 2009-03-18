789821276b5c79c73050ce46c8662c605449f7e4
Markus Bodden <bom@comnets.rwth-aachen.de>
2009-03-18 22:54:50 +0100

    Replaced header comments for public part licencesing

--- src/TCPHeader.hpp
+++ src/TCPHeader.hpp
@@ -1,12 +1,28 @@
-/******************************************************************************
- * TCPModule   Internet Protocol Implementation                               *
- * __________________________________________________________________________ *
- *                                                                            *
- * Copyright (C) 2008                                                         *
- * Lehrstuhl fuer Kommunikationsnetze (ComNets)                               *
- * Kopernikusstr. 16, D-52074 Aachen, Germany                                 *
- * phone: ++49-241-80-27910 (phone), fax: ++49-241-80-22242                   *
- * email: wns@comnetsrwth-aachen.de, www: http://wns.comnets.rwth-aachen.de/  *
+/*******************************************************************************
+ * This file is part of openWNS (open Wireless Network Simulator)
+ * _____________________________________________________________________________
+ *
+ * Copyright (C) 2004-2007
+ * Chair of Communication Networks (ComNets)
+ * Kopernikusstr. 16, D-52074 Aachen, Germany
+ * phone: ++49-241-80-27910,
+ * fax: ++49-241-80-22242
+ * email: info@openwns.org
+ * www: http://www.openwns.org
+ * _____________________________________________________________________________
+ *
+ * openWNS is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License version 2 as published by the
+ * Free Software Foundation;
+ *
+ * openWNS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
  ******************************************************************************/
 
 #ifndef TCP_TCPHEADER_HPP
